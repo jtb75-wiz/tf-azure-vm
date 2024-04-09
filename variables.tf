b28fec397271ef349bd4966100bb3ee8326fec76
jtb75-wiz <153871848+jtb75-wiz@users.noreply.github.com>
2024-04-09 15:34:30 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
